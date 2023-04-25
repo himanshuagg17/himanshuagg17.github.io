f8888f9a68cfc4e97143b8d25ab81fcf46db5d84
himanshuabd17 <himanshu12061998@gmail.com>
2023-04-25 10:16:48 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,61 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+
+const resume = document.getElementById("resume-button-1");
+resume.onclick = (e) => {
+    // e.preventDefault()
+    window.open("./Media/Himanshu-Aggarwal-Resume.pdf")
+    // console.log("Click");
+}
+
+const containerResume = document.getElementById("resume-button-2");
+containerResume.onclick = (e) => {
+    // e.preventDefault()
+    window.open("./Media/Himanshu-Aggarwal-Resume.pdf")
+    // console.log("Click");
+}
+                          
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
